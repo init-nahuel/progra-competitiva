70c3cea4fec0732e3390a6a7215d57be0e0f1bf6
Nahuel <nahuelmath@gmail.com>
2023-03-18 17:47:06 -0300

    add: valid solution

--- ejercicios/facil/torres_bellisimas.cpp
+++ ejercicios/facil/torres_bellisimas.cpp
@@ -3,47 +3,6 @@
 
 using namespace std;
 
-vector<int> to_bit(string w) {
-    int n = w.length();
-    vector<int> bit(n);
-    for (int i=0; i<n; i++) {
-        bit[i] = (w[i] == 'R') ? 1 : 0;
-    }
-    return bit;
-}
-
-vector<int> xor_op(vector<int> bit1, vector<int> bit2) {
-    int n = bit1.size(), m = bit2.size(), min = (n<m) ? n : m;
-    vector<int> res(min, 0); // Se puede hacer con el minimo tmbn
-    if (n>m) {
-        for (int i=0; i<m; i++) {
-            res[i] = bit1[i+m-1] ^ bit2[i];
-        }
-    }
-    else {
-        for (int i=0; i<n; i++) {
-            res[i] = bit1[i] ^ bit2[i+n-1];
-        }
-
-    }
-    return res;
-}
-
-void solve() {
-    int n, m; cin >> n >> m;
-    string s, t; cin >> s >> t;
-    // Usar bitwise: Transformar los strings a bits que sean la representacion de este
-    // R -> 1 y B -> 0, Ej: RBRB -> 1010
-    // BRBB -> 0100,  RBR -> 101
-    // OUTPUT: 0100 XOR _101 = _001 
-    // BRBRR -> 01011,  BRBR -> 0101
-    // OUTPUT: 01011 XOR 0101 = _1110
-    // El XOR debe empezar el bit con 1, si empieza con 0 entonces la torre no es bellisima
-    vector<int> bit_s = to_bit(s), bit_t = to_bit(t), bit_xor = xor_op(bit_s, bit_t);
-    string res = (bit_xor[min(bit_s.size(), bit_t.size())-1] == 1) ? "NO" : "YES";
-    cout << res << "\n";
-}
-
 int is_beautiful(string w) {
     int n = w.length();
     if (n == 1) return 1;
@@ -59,15 +18,28 @@ void solve() {
     int n, m; cin >> n >> m;
     string s, t; cin >> s >> t;
     string res = "NO";
-    while (n>0 && m>0) {
-        if (is_beautiful(s) && is_beautiful(t)) {
-            res = "YES"; break;
-        }
-        else if (!is_beautiful(s) && !is_beautiful(t)) break;
-        else if (!is_beautiful(s) && is_beautiful(t)) {
-            // TODO: Completar :D
+    if (is_beautiful(s) && is_beautiful(t)) {
+        res = "YES";
+    }
+    else if (!is_beautiful(s) && is_beautiful(t)) {
+        while(n>1) {
+            t.push_back(s[n-1]);
+            s.pop_back(); n--;
+            if (is_beautiful(s) && is_beautiful(t)) {
+                res = "YES"; break;
+            }
         }
     }
+    else if (is_beautiful(s) && !is_beautiful(t)) {
+        while (m>1) {
+            s.push_back(t[m-1]);
+            t.pop_back(); m--;
+            if (is_beautiful(s) && is_beautiful(t)) {
+                res = "YES"; break;
+            }
+        }
+    }
+
     cout << res << "\n";
 }
 
