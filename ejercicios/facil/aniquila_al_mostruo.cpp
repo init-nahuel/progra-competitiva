7c15de60671e81a439cf1d9be99c2f85729449a2
Nahuel <nahuelmath@gmail.com>
2023-03-19 15:33:07 -0300

    TODO: complete solution

---
+++ ejercicios/facil/aniquila_al_mostruo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int hc, dc; cin >> hc >> dc;
+    int hm, dm; cin >> hm >> dm;
+    int k, w, a; cin >> k >> w >> a;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) solve(); 
+    return 0;
+}
