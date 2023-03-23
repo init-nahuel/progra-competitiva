3d26e74391c33d5fc9ebed2a225f97cb7921372d
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-03-22 22:46:43 -0300

    refactor: incomplete solution (faile test 2)

--- ejercicios/facil/aniquila_al_mostruo.cpp
+++ ejercicios/facil/aniquila_al_mostruo.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 void solve() {
-    int hc, dc; cin >> hc >> dc;
+    long long hc, dc; cin >> hc >> dc;
     int hm, dm; cin >> hm >> dm;
     int k, w, a; cin >> k >> w >> a;
+    double c_turns = ceil(hc/dm), m_turns = ceil(hm/dc);
+    string res = "NO";
+    
+    if (c_turns >= m_turns) res = "YES";
+    else {
+        while (c_turns < m_turns) {
+            for (int i=k; i>=0; i--) {
+                for (int j=0;j<=k; j++) {
+                    double new_hc = hc+j*a, new_dc = dc+i*w;
+                    c_turns = ceil(new_hc/dm);
+                    m_turns = ceil(hm/new_dc);
+                }
+                if (c_turns >= m_turns) {
+                    res = "YES"; break;
+                }
+            }
+            break;
+        }
+    }
+    cout << res << "\n";
 }
 
 int main() {
