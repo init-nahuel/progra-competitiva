65eba505c3a5e70479d30ecef870371ff52a961a
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-03-28 02:29:47 -0300

    add: incomplete solution

--- ejercicios/libreria_estandar/quiero_mis_ubs.cpp
+++ ejercicios/libreria_estandar/quiero_mis_ubs.cpp
@@ -14,24 +14,68 @@ y luego bastaria con ver si ese par se encuentra en esta lista (tomando el menor
 de la tupla)
 */
 
-void solve() {
-    // s: num de departamentos, l: num de pares que se llevan bien, n: num de aux en fila
+vector<pair<string, string>> lexicographical_transform(vector<pair<string, string>> *pair_deps, int size) {
+    for (int i=0; i<size; i++) {
+        string str1 = (*pair_deps)[i].first, str2 = (*pair_deps)[i].second;
+        if (str2.compare(str1) < 0) {
+            pair<string, string> p = {str2, str1}; (*pair_deps)[i] = p;
+        }
+    }
+    return *pair_deps;
+}
+
+int friends(string str1, string str2, vector<pair<string, string>> pair_deps, int size) {
+    for (int i=0; i<size; i++) {
+        pair<string, string> p = pair_deps[i];
+        if (p.first == str1 && p.second == str2) return 1;
+    }
+    return 0;
+}
+
+vector<string> solve(int s, int l, int n, vector<pair<string, string>> pair_deps, vector<string> *auxs_line) {
+    pair_deps = lexicographical_transform(&pair_deps, l);
+    for (int i=n-2; i>=0; i++) {
+        string str1 = (*auxs_line)[i], str2 = (*auxs_line)[i+1];
+        int j = i;
+        while (friends(str2, str1, pair_deps, l) && j>=0) {
+            (*auxs_line)[j] = str2; (*auxs_line)[j+1] = str1;
+            str1 = (*auxs_line)[j-1], str2 = (*auxs_line)[j];
+            j--;
+        }
+        // if (friends(str2, str1, pair_deps, l)) {
+        //     (*auxs_line)[i] = str2; (*auxs_line)[i+1] = str1;
+        // }
+    }
+    return *auxs_line;
+}
+
+int main() {
+    // s: num de departamentos, l: num de pares que se llevan bien, n: num de auxs en fila
     int s, l, n; cin >> s >> l >> n;
     vector<string> deps;
-    vector<string> pair_deps;
-    for (int i=0; i<n ;i++) {
+    vector<pair<string, string>> pair_deps;
+    vector<string> auxs_line;
+    for (int i=0; i<s; i++) {
         string dep; cin >> dep;
         deps.push_back(dep);
     }
     for (int i=0; i<l; i++) {
-        pair<string, string> pair;
-        cin >> pair.first >> pair.second;
+        pair<string, string> p; cin >> p.first >> p.second;
+        pair_deps.push_back(p);
     }
-}
+    for (int i=0; i<n; i++) {
+        string aux; cin >> aux;
+        auxs_line.push_back(aux);
+    }
 
-int main() {
-    // solve();
-    string s;
-    cin >> s; cout << s << endl;
+    if (s==1 || l==0 || n==1) for (int i=0; i<n; i++) cout << auxs_line[i] << "\n";
+    else {
+        vector<string> new_line = solve(s, l, n, pair_deps, &auxs_line);
+        for (int i=0; i<n; i++) {
+            cout << new_line[i] << " ";
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
