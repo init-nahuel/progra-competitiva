44a147c2a790cd990d9b0b0bf84de3209dc203e3
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-05-11 11:59:15 -0400

    solved >: (version: c++ 8.3)

--- ejercicios/grafos_2/mejor_camino.cpp
+++ ejercicios/grafos_2/mejor_camino.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 ll INF = 1e18;
 
 vector <ll> dijkstra(vector<vector<pair<int, ll>>> adj, int size, int start, int end) {
-    using state = pair<int, ll>;
+    using state = pair<ll, int>;
     priority_queue <state, vector<state>, greater<state>> pq;
     vector<ll> dist(size, INF);
 
@@ -13,12 +13,15 @@ vector <ll> dijkstra(vector<vector<pair<int, ll>>> adj, int size, int start, int
     dist[start] = 0;
 
     while (!pq.empty()) {
-        auto [weight, u] = pq.top();
+        pair<ll, int> t = pq.top();
+        ll weight = t.first;
+        int u = t.second;
         pq.pop();
 
         if (weight != dist[u]) continue;
 
-        for (auto [v, w]: adj[u]) {
+        for (pair<int, ll> p: adj[u]) {
+            int v = p.first; ll w = p.second;
             if (weight + w < dist[v]) {
                 dist[v] = weight + w;
                 pq.push({dist[v], v});
@@ -35,10 +38,11 @@ int main() {
         int n, m; cin >> n >> m;
         if (n == 0 && m == 0) break;
         vector<vector<pair<int, ll>>> adj(n), reverse_adj(n);
-        vector<tuple<int, int, ll>> edges(n);
+        vector<tuple<int, int, ll>> edges(m);
         int s, d; cin >> s >> d;
         for (int i=0; i<m; i++) {
-            int u, v, p; cin >> u >> v >> p;
+            ll p;
+            int u, v; cin >> u >> v >> p;
             adj[u].push_back({v, p});
             reverse_adj[v].push_back({u, p});
             edges[i] = {u, v, p};
@@ -47,9 +51,11 @@ int main() {
         vector<ll> dijkstra_reverse_graph = dijkstra(reverse_adj, n, d, s);
         int short_distance = dijkstra_graph[d];
 
-        for (auto [u, v, p]: edges) {
+        for (tuple<int, int, ll> t: edges) {
+            int u = get<0>(t), v = get<1>(t);
+            ll p = get<2>(t);
             if (dijkstra_graph[u] + p + dijkstra_reverse_graph[v] == short_distance) {
-                adj[u].erase(find(adj[u].begin(), adj[u].end(), pair(v, p)));
+                adj[u].erase(find(adj[u].begin(), adj[u].end(), make_pair(v, p)));
             }
         }
 
