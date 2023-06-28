190c8de5e12e844fe0f147e725e210c0072084df
Nahuel <nahuelmath@gmail.com>
2023-06-28 19:46:04 -0400

    add: iterator distance getter, need fix

--- ejercicios/teoria_numeros/dim_truco.cpp
+++ ejercicios/teoria_numeros/dim_truco.cpp
@@ -1,27 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> modified_sieve(int n) {
+set<int> modified_sieve(int n) {
     vector<int> sieve(n, 0);
+    set<int> divs_3;
     int counter = 0;
     for (int i=2; i<n; i++) {
         if (sieve[i]) continue;
-        int div_3 = i*i;
-        if (div_3 >= n) break;
-        sieve[div_3] = 1;
+        divs_3.insert(i*i);
+        for (int j=i; j<n; j+=i) {
+            sieve[j] = j;
+        }
     }
 
-    return sieve;
+    return divs_3;
 }
 
-pair<int, int> solve(int n, int k, vector<int> sieve) {
-    
-    return make_pair(count(sieve.begin(), sieve.begin()+n, 1), count(sieve.begin()+k+1, sieve.begin()+n, 1));
+pair<int, int> solve(int n, int k, set<int> sieve) {
+    int len_sieve = size(sieve);
+    auto it_n = sieve.lower_bound(n);
+    auto it_k = sieve.lower_bound(k);
+    int divs_3 = distance(sieve.begin(), it_n);
+    int great_k = distance(it_k, sieve.end());
+    return make_pair(divs_3, great_k);
 }
 
 int main() {
     int t; cin >> t;
-    vector<int> sieve = modified_sieve(100000);
+    set<int> sieve = modified_sieve(100000);
     while (t--) {
         int n, k; cin >> n >> k;
         if (n < 4 || k > n) {
