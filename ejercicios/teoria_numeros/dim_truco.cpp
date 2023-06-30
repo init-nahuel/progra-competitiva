ef63a032421c0d94b60fabb43cecf1f5c42c8af2
Nahuel <nahuelmath@gmail.com>
2023-06-30 01:07:34 -0400

    add: solution with bs

--- ejercicios/teoria_numeros/dim_truco.cpp
+++ ejercicios/teoria_numeros/dim_truco.cpp
@@ -3,28 +3,50 @@ using namespace std;
 
 vector<int> modified_sieve(int n) {
     vector<int> sieve(n+1, 0);
-    int counter = 0;
+    vector<int> squares_prime;
     for (int i=2; i<=n; i++) {
         if (sieve[i]) continue;
-        counter +=  1;
         for (int j=i*2; j<=n; j+=i) {
-            if (j >= i*i) {
-                sieve[j] = counter;
+            if (j == i*i) {
+                sieve[j] = i*i;
+                squares_prime.push_back(i*i);
                 continue;
             }
             sieve[j] = i;
         }
     }
 
-    return sieve;
+    return squares_prime;
 }
 
-pair<int, int> solve(int n, int k, vector<int> &sieve) {
+pair<int, int> solve(int n, int k, vector<int> sieve) {
     int sqrt_n = (int) floor(sqrt(n)), sqrt_k = (int) floor(sqrt(k));
-    int divs_3 = sieve[sqrt_n*sqrt_n];
-    int divs_great_k = divs_3 - sieve[sqrt_k*sqrt_k];
+    // int divs_3 = sieve[sqrt_n*sqrt_n];
+    // int divs_great_k = divs_3 - sieve[sqrt_k*sqrt_k];
+    int len = size(sieve);
+    int bin_n = binary_search(sieve, sqrt_n, 0, n);
+    int bin_k = binary_search(sieve, sqrt_k, 0, n);
     
-    return make_pair(divs_3, divs_great_k);
+    return make_pair(n - bin_n, bin_n - bin_k);
+}
+
+int binary_search(vector<int> arr, int val, int l, int r) {
+    while (l <= r) {
+        int mid = (l+r)/2;
+        if (l == r) {
+            return l;
+        }
+        if (arr[mid] > val) {
+            r = mid;
+        }
+        else if (arr[mid] == val) {
+            return mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+    return l;
 }
 
 int main() {
